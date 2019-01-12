d50a012a7d5ca56e6eb5f6aed5007c3b5d4cc5e6
Tunti1 <45206192+Tunti1@users.noreply.github.com>
2019-01-12 14:12:17 +0100

    Add files via upload

---
+++ bin/Debug/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Uredjaj	]
+(
+	[Sifra] INT NOT NULL PRIMARY KEY, 
+    [Marka] VARCHAR(50) NOT NULL, 
+    [Model ] VARCHAR(50) NOT NULL, 
+    [Komponenta] VARCHAR(MAX) NOT NULL, 
+    [Rok] DATE NOT NULL
+)
